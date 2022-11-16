b03e1bdae6d5febed1839efd732d5ade5c5c73f6
Sergio Martin Rubio <me@sergiomartinrubio.com>
2022-11-16 20:32:21 +0100

    Verify contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,37 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
-  const MyContractFactory = await ethers.getContractFactory("MyContract")
-  console.log("Deploying contract...")
-  const myContract = await MyContractFactory.deploy()
-  await myContract.deployed()
-  console.log(`Deployed contract to: ${myContract.address}`)
+    const MyContractFactory = await ethers.getContractFactory("MyContract")
+    console.log("Deploying contract...")
+    const myContract = await MyContractFactory.deploy()
+    await myContract.deployed()
+    console.log(`Deployed contract to: ${myContract.address}`)
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await myContract.deployTransaction.wait(6) // make sure the contract is already on Etherscan
+        await verify(myContract.address, [])
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        // we continue in case something goes wrong
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
